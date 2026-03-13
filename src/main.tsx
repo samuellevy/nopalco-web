94c3c248c813ce963387865c5af5cae43562116b
Samuel Levy <samuellevy.dev@gmail.com>
2026-03-13 15:07:07 -0300

    fix: problm on cache

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+// limpar service workers antigos
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then((registrations) => {
+      registrations.forEach((reg) => reg.unregister());
+    })
+    .catch(() => {});
+}
+
+// limpar caches
+if ('caches' in window) {
+  caches
+    .keys()
+    .then((names) => {
+      names.forEach((name) => caches.delete(name));
+    })
+    .catch(() => {});
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
